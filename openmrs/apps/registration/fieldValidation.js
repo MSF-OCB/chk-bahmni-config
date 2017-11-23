19214106b5d139764ee27060d715f28cb9d8c251
Pritam Das <dpritam@thoughtworks.com>
2017-11-23 11:49:00 +0530

    #114 Pritam | Reg Feedback
    When entering a patient "date de depistage" the system has accepted a date that is in the future

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -1,20 +1,8 @@
 Bahmni.Registration.customValidator = {
-    "age.days": {
-        method: function (name, value) {
-            return value >= 0;
+    "Date depistage": {
+        method: function (name,value,personAttributeDetails) {
+             return value <= new Date();
         },
-        errorMessage: "REGISTRATION_AGE_ERROR_KEY"
-    },
-    "Telephone Number": {
-        method: function (name, value, personAttributeDetails) {
-            return value && value.length> 6;
-        },
-        errorMessage: "REGISTRATION_TELEPHONE_NUMBER_ERROR_KEY"
-    },
-    "caste": {
-        method: function (name, value, personAttributeDetails) {
-            return value.match(/^\w+$/);
-        },
-        errorMessage: "REGISTRATION_CASTE_TEXT_ERROR_KEY"
+     errorMessage: "REGISTRATION_HIV_DATE_ERROR_KEY"
     }
 };
