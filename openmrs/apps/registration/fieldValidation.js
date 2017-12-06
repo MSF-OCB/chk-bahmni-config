515b784b67bd1a71528115add4b3e68065a83414
Yash <ydave@thoughtworks.com>
2017-12-06 17:42:45 +0530

    Yash|Configuration of Attributes to record Patient Dependents and Patient Confidents on Reg Page

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -4,5 +4,11 @@ Bahmni.Registration.customValidator = {
              return value <= new Date();
         },
      errorMessage: "REGISTRATION_HIV_DATE_ERROR_KEY"
+    },
+    "Date de naissance_Dep(Age c'est plus simple)": {
+        method: function (name,value,personAttributeDetails) {
+             return value <= new Date();
+        },
+     errorMessage: "REGISTRATION_HIV_DATE_ERROR_KEY"
     }
 };
