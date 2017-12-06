98912cf4b5185a02f8b5d35961c341f87d15701f
Yash <ydave@thoughtworks.com>
2017-12-06 21:27:50 +0530

    Yash|Registration Page related Changes

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -5,7 +5,7 @@ Bahmni.Registration.customValidator = {
         },
      errorMessage: "REGISTRATION_HIV_DATE_ERROR_KEY"
     },
-    "Date de naissance_Dep(Age c'est plus simple)": {
+    "Date de naissance_Dep": {
         method: function (name,value,personAttributeDetails) {
              return value <= new Date();
         },
