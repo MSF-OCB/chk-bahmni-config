38d55f3d2a6ad463f3c061f7622bf83585e418dd
RdN-MSF <ramses.de.norre@brussels.msf.org>
2017-12-08 09:33:58 +0100

    Limit the identifier to either 5 or 6 digits.

--- sql-scripts/R1.0.sql
+++ sql-scripts/R1.0.sql
@@ -2,3 +2,4 @@ SET character_set_client = 'utf8';
 update concept_name set name ="Rép. du Congo-Brazzaville" where name = "Rép. du Congo";
 update  concept_name set name="Province d'origine" where name="Province de provenance";
 Update person_attribute_type set name="Province d'origine" where name="Province de provenance";
+
