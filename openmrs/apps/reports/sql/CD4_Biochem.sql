00160d38d304977f9cd75985f3df03dbd7628c0a
matirp235 <dpritam@thoughtworks.com>
2017-12-19 14:16:09 +0530

    Pritam | Final modification in reports

--- openmrs/apps/reports/sql/CD4_Biochem.sql
+++ openmrs/apps/reports/sql/CD4_Biochem.sql
@@ -1,21 +1,22 @@
-SELECT B.date_of_results AS "Date of Results",
+SELECT B.date_of_results :: date  AS "Date of Results",
        B.care_center_requesting AS "Center",
        B.Patient_Name AS "Nom du patient",
        B.Patient_Identifier AS "Patient ID",
        EXTRACT(YEAR FROM Now()) - EXTRACT(YEAR FROM B.dob) as "Age"  ,
        B.sexe AS "Sexe",
-       sum(cast(B.CD4 AS INTEGER)) AS "RESULTS CD4 (cells/µl)",
-       sum(cast(B.CD4_percentage AS INTEGER)) AS "CD4 %",
-       sum(cast(B.GPT AS INTEGER)) AS "SGPT (UI/L)",
-       sum(cast(B.Creatinine AS INTEGER)) AS "CREAT (µmol/L)",
-       sum(cast(B.Glucose_LCR AS INTEGER)) AS "GLU LCR (mg/dl)",
-       (Select dict_entry from DICTIONARY where id in (sum(cast(B.Hep_B AS INTEGER))) ) "Hep. B", /*getting coded value for tests*/
-       (Select dict_entry from DICTIONARY where id in (sum(cast(B.Crag_serique AS INTEGER))) )AS "CRAG SERIQUE",/*getting coded value for tests*/
-       (Select dict_entry from DICTIONARY where id in (sum(cast(B.Crag_LCR AS INTEGER)))) AS "CRAG LCR"/*getting coded value for tests*/
+       sum(cast(B.CD4 AS NUMERIC)) AS "RESULTS CD4 (cells/µl)",
+       sum(cast(B.CD4_percentage AS NUMERIC)) AS "CD4 %",
+       sum(cast(B.GPT AS NUMERIC)) AS "SGPT (UI/L)",
+       sum(cast(B.Creatinine AS NUMERIC)) AS "CREAT (µmol/L)",
+       sum(cast(B.Glucose_LCR AS NUMERIC)) AS "GLU LCR (mg/dl)",
+       (Select dict_entry from DICTIONARY where id in (sum(cast(B.Hep_B AS NUMERIC))) ) "Hep. B", /*getting coded value for tests*/
+       (Select dict_entry from DICTIONARY where id in (sum(cast(B.Crag_serique AS NUMERIC))) )AS "CRAG SERIQUE",/*getting coded value for tests*/
+       (Select dict_entry from DICTIONARY where id in (sum(cast(B.Crag_LCR AS NUMERIC)))) AS "CRAG LCR"/*getting coded value for tests*/
 
 FROM
   (/*Pivoting the table row to column*/
       SELECT
+       accession_number,
        date_of_results,
        Patient_Name,
        care_center_requesting,
@@ -48,52 +49,55 @@ FROM
        END AS Crag_LCR
 
        FROM
-         (/*defining columns*/          SELECT r.lastupdated :: DATE AS date_of_results,
-                                                      ss.name AS care_center_requesting,
-                                                      trim( concat( COALESCE(NULLIF(person.first_name, ''), ''), ' ', COALESCE(NULLIF(person.last_name, ''), '') ) ) AS Patient_Name,
-                                                      pi.identity_data AS Patient_Identifier,
-                                                      patient.birth_date :: DATE AS dob,
-                                                      patient.gender AS sexe,
-                                                      t.name AS tname,
-                                                      r.value AS tvalue
-
-
+         (/*defining columns*/
+           SELECT
+                sample.lastupdated  AS date_of_results,
+                ss.name AS care_center_requesting,
+                trim( concat( COALESCE(NULLIF(person.first_name, ''), ''), ' ', COALESCE(NULLIF(person.last_name, ''), '') ) ) AS Patient_Name,
+                pi.identity_data AS Patient_Identifier,
+                patient.birth_date :: DATE AS dob,
+                case when patient.gender = 'M' then 'H'
+                     when patient.gender = 'F' then 'F'
+                     when patient.gender = 'O' then 'A'
+                else
+                    patient.gender END AS sexe,
+                t.name AS tname,
+                r.value AS tvalue,
+                sample.accession_number
           FROM
-            (                           SELECT p_identiity.identity_data,
-                                                         p_identiity.patient_id
-             FROM patient_identity p_identiity
-             WHERE p_identiity.identity_type_id = 2) pi
+          patient_identity pi
           INNER JOIN patient ON patient.id = pi.patient_id
           INNER JOIN person ON patient.person_id = person.id
           INNER JOIN sample_human ON patient.id = sample_human.patient_id
-          INNER JOIN
-            ( /* Selecting order which are validated ie status = 3*/
-            SELECT id, sample_source_id
-            FROM sample
-            WHERE accession_number IS NOT NULL
-            and status_id=3
-            ) AS sample ON sample_human.samp_id = sample.id
+          INNER JOIN sample sample on sample_human.samp_id=sample.id
           INNER JOIN sample_source ss ON sample.sample_source_id = ss.id
           INNER JOIN sample_item item ON sample.id = item.samp_id
           INNER JOIN analysis a ON item.id = a.sampitem_id
           INNER JOIN RESULT r ON a.id = r.analysis_id
           INNER JOIN test t ON a.test_id = t.id
-          WHERE t.name IN ('CD4',
-                           'CD4 % (enfants de - 5 ans)',
-                           'GPT',
+          WHERE t.name IN (
+                            'CD4',
+                            'CD4 % (enfants de - 5 ans)',
+                            'GPT',
                             'Creatinine',
                             'Glucose (LCR)',
                             'Hep. B',
                             'Crag serique',
-                            'Crag') ) AS A
+                            'Crag'
+                          )
+                AND a.status_id=6 /*Filtering the result which are validated*/
+                AND sample.accession_number IS NOT NULL
+                AND pi.identity_type_id = 2
+                           ) AS A
     ) AS B
-    WHERE B.date_of_results BETWEEN '#startDate#' and '#endDate#'
+WHERE date(B.date_of_results) BETWEEN '#startDate#' and '#endDate#'
 GROUP BY B.Patient_Name,
          B.care_center_requesting,
          B.Patient_Identifier,
          B.dob,
          B.sexe,
-         B.date_of_results
+         B.date_of_results,
+         B.accession_number
 ORDER BY B.date_of_results,
          B.care_center_requesting,
          B.Patient_Name,
