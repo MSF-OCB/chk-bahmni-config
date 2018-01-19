d053f79c1b6243145b9545c768ca61b4036ae69e
Mahesh Kumar <maheshkv@thoughtworks.com>
2018-01-19 17:51:06 +0530

    Mahesh,Ravi | Adding sql scripts (added by Ramses) to migrations

---
+++ openmrs/migrations/R1.0.sql
@@ -0,0 +1,5 @@
+SET character_set_client = 'utf8';
+update concept_name set name="Rép. du Congo-Brazzaville" where name="Rép. du Congo" and concept_name_type="FULLY_SPECIFIED";
+update  concept_name set name="Province d'origine" where name="Province de provenance" and concept_name_type="FULLY_SPECIFIED";
+update person_attribute_type SET NAME="Province d'origine" where  name="Province de provenance" and retired = 0;
+
