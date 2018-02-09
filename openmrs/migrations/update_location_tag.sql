e84b9293beb05f76177b000076f9ee6e99b7becb
vishnu <vishnu@indvishnu.local>
2018-02-09 10:27:53 +0530

    Vishnu|Adding the xml file for location_tag for IPD module

--- openmrs/migrations/update_location_tag.sql
+++
@@ -1,3 +0,0 @@
-          set @tagid =(select location_tag_id from location_tag where name='Admission Location');
-          set @locationid =(select location_id from location where name ='CHK');
-          insert into location_tag_map (location_id,location_tag_id) values (@locationid,@tagid);
