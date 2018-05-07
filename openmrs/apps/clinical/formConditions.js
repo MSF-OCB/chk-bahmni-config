2a7f5c686d35533ac6385f75a760117ade404a60
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-05-07 15:22:19 +0530

    Adding the Paraclinique form for R3

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -33,5 +33,48 @@ Bahmni.ConceptSet.FormConditions.rules = {
           conditions.disable.push("Resultat(Creatine)","Resultat")
         }
         return conditions;
-    }
+    },
+    'Mis sous ARV en hospitalization': function (formName, formFieldValues) {
+            var conditions = {
+                show: [],
+                hide: []
+            };
+
+            var MisSous = "Date initiation";
+            var result = formFieldValues['Mis sous ARV en hospitalization'];
+            if (result == "Oui") {
+                conditions.show.push( MisSous);
+            } else {
+                conditions.hide.push( MisSous);
+            }
+            return conditions;
+        },
+    'Traitemtent TB commencé?': function (formName, formFieldValues) {
+                    var conditions = {
+                        show: [],
+                        hide: []
+                    };
+
+                    var Traitemtent = "Date début";
+                    var result = formFieldValues['Traitemtent TB commencé?'];
+                    if (result == "Oui") {
+                        conditions.show.push( Traitemtent);
+                    } else {
+                        conditions.hide.push( Traitemtent);
+                    }
+                    return conditions;
+                },
+'Type d\'Echo' : function (formName, formFieldValues) {
+                    var form_variable = formFieldValues['Type d\'Echo'];
+             
+                    if (form_variable == "Autre") {
+                        return {
+                            enable: ["Autre Type d\’Echo"]
+                        }
+                    } else {
+                        return {
+                            disable: ["Autre Type d\’Echo"]
+                        }
+                    }
+                }
  };
