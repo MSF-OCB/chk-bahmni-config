6daf02cb73c6f47cf48473eb8e5920703f9ef8bd
vishnu <vishnu@indvishnu.local>
2018-05-08 08:54:08 +0530

    Vishnu,Sudhamsh fixing the change for not to display empty strings

--- openmrs/apps/reports/sql/CD4_Biochem.sql
+++ openmrs/apps/reports/sql/CD4_Biochem.sql
@@ -1,18 +1,21 @@
-SELECT to_char(B.date_of_results,'DD/MM/YYYY')   AS "Date des résultats",
-       B.care_center_requesting AS "Provenance",
+Select
+*
+ from(
+      Select  B.care_center_requesting AS "Provenance",
        B.Patient_Name AS "Nom du patient",
        B.Patient_Identifier AS "ID Patient",
        EXTRACT(YEAR FROM Now()) - EXTRACT(YEAR FROM B.dob) as "Age"  ,
        B.sexe AS "Sexe",
        to_char(B.sample_date, 'DD/MM/YYYY') as "Date de prélèvement",
-       sum(cast( CASE when B.CD4 = '' then NULL ELSE B.CD4 END  AS NUMERIC)) AS "RESULTS CD4 (cells/µl)",
-       sum(cast( CASE when B.CD4_percentage = '' then NULL ELSE B.CD4_percentage END  AS NUMERIC)) AS "CD4 %",
-       sum(cast( CASE when B.GPT = '' then NULL ELSE B.GPT END AS NUMERIC)) AS "SGPT (UI/L)",
-       sum(cast( CASE when B.Creatinine = '' then NULL ELSE B.Creatinine END  AS NUMERIC)) AS "CREAT (µmol/L)",
-       sum(cast( CASE when B.Glucose_LCR = '' then NULL ELSE B.Glucose_LCR END  AS NUMERIC)) AS "GLU LCR (mg/dl)",
-       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Hep_B = '' then NULL ELSE B.Hep_B END AS NUMERIC))) ) "Hep. B", /*getting coded value for tests*/
-       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Crag_serique = '' then NULL ELSE B.Crag_serique END AS NUMERIC))) )AS "CRAG SERIQUE",/*getting coded value for tests*/
-       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Crag_LCR = '' then NULL ELSE B.Crag_LCR END AS NUMERIC)))) AS "CRAG LCR"/*getting coded value for tests*/
+
+       sum(cast( CASE when B.CD4 = '' then null  ELSE B.CD4 END  AS NUMERIC)) AS "RESULTS CD4 (cells/µl)",
+       sum(cast( CASE when B.CD4_percentage = '' then null ELSE B.CD4_percentage END  AS NUMERIC)) AS "CD4 %",
+       sum(cast( CASE when B.GPT = '' then null ELSE B.GPT END AS NUMERIC)) AS "SGPT (UI/L)",
+       sum(cast( CASE when B.Creatinine = '' then null ELSE B.Creatinine END  AS NUMERIC)) AS "CREAT (µmol/L)",
+       sum(cast( CASE when B.Glucose_LCR = '' then null ELSE B.Glucose_LCR END  AS NUMERIC)) AS "GLU LCR (mg/dl)",
+       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Hep_B = '' then null ELSE B.Hep_B END AS NUMERIC))) ) "Hep. B", /*getting coded value for tests*/
+       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Crag_serique = '' then null  ELSE B.Crag_serique END AS NUMERIC))) )AS "CRAG SERIQUE",/*getting coded value for tests*/
+       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Crag_LCR = '' then null  ELSE B.Crag_LCR END AS NUMERIC)))) AS "CRAG LCR"/*getting coded value for tests*/
 
 FROM
   (/*Pivoting the table row to column*/
@@ -25,10 +28,10 @@ FROM
        dob,
        sexe,
        sample_date,
-       
+
        CASE
            WHEN tname ='CD4' THEN tvalue
-       END AS CD4,
+       END AS CD4 ,
        CASE
            WHEN tname ='CD4 % (enfants de - 5 ans)' THEN tvalue
        END AS CD4_percentage,
@@ -42,7 +45,7 @@ FROM
            WHEN tname ='Glucose (LCR)' THEN tvalue
        END AS Glucose_LCR,
        CASE
-           WHEN tname ='Hep. B' THEN tvalue
+           WHEN tname ='Hépatite B' THEN tvalue
        END AS Hep_B,
        CASE
            WHEN tname ='Crag serique' THEN tvalue
@@ -85,16 +88,19 @@ FROM
                             'GPT',
                             'Creatinine',
                             'Glucose (LCR)',
-                            'Hep. B',
+                            'Hépatite B',
                             'Crag serique',
                             'Crag'
                           )
+
                 AND a.status_id=6 /*Filtering the result which are validated*/
                 AND sample.accession_number IS NOT NULL
                 AND pi.identity_type_id = 2
+
                            ) AS A
     ) AS B
 WHERE date(B.date_of_results) BETWEEN '#startDate#' and '#endDate#'
+
 GROUP BY B.Patient_Name,
          B.care_center_requesting,
          B.Patient_Identifier,
@@ -103,8 +109,15 @@ GROUP BY B.Patient_Name,
          B.sample_date,
          B.date_of_results,
          B.accession_number
+
 ORDER BY B.date_of_results,
          B.care_center_requesting,
          B.Patient_Name,
          B.dob,
-         B.sexe;
+         B.sexe
+
+
+         ) as A
+         Where
+coalesce ("RESULTS CD4 (cells/µl)", "CD4 %","SGPT (UI/L)","CREAT (µmol/L)","GLU LCR (mg/dl)") is not null
+and coalesce ("CRAG LCR","Hep. B","CRAG SERIQUE") is not null;
