7022a65aeb3a8506071f7dc2bd606dd0c40946dc
matirp235 <dpritam@thoughtworks.com>
2018-05-24 12:48:27 +0530

    Pritam | #271 Feedback change

--- openmrs/apps/reports/sql/PerdusDeVueEtPresumesPerDusDeVue.sql
+++ openmrs/apps/reports/sql/PerdusDeVueEtPresumesPerDusDeVue.sql
@@ -1,5 +1,4 @@
-select
-identifier,
+SELECT identifier,
 group_concat( distinct ( case when personAttributeTypeDetails.name="Type de cohorte" then cvForAttribute.concept_full_name else NULL end )) as "Type cohorte",
 concat( COALESCE(NULLIF(personalDetails.given_name, ''), ''), ' ', COALESCE(NULLIF(personalDetails.family_name, ''), '') ) AS "Nom",
 concat(floor(datediff(now(), personForDetails.birthdate)/365), ' ans, ',  floor((datediff(now(), personForDetails.birthdate)%365)/30),' mois') AS "Age",
@@ -8,7 +7,6 @@ CASE WHEN personForDetails.gender = 'M' THEN 'H'
      WHEN personForDetails.gender = 'F' THEN 'F'
      WHEN personForDetails.gender = 'O' THEN 'A'
      else personForDetails.gender END AS "Sexe",
-
 group_concat( distinct ( case when personAttributeTypeDetails.name="Date entrée cohorte"
                               then date_format(DATE(personAttributeDetails.value), '%d-%m-%Y')
                               else NULL end )) as "Date entrée cohorte",
@@ -28,6 +26,8 @@ group_concat( distinct ( case when personAttributeTypeDetails.name="Nom du Confi
 group_concat( distinct ( case when personAttributeTypeDetails.name="Tel Conf 1" then personAttributeDetails.value else NULL end )) as "Téléphone confident 1",
 group_concat( distinct ( case when personAttributeTypeDetails.name="Nom du Confident 2" then personAttributeDetails.value else NULL end )) as "Nom confident 2",
 group_concat( distinct ( case when personAttributeTypeDetails.name="Tel Conf 2" then personAttributeDetails.value else NULL end )) as "Téléphone confident 2",
+
+
 CASE when personForDetails.dead = 0 then "Non"
      When personForDetails.dead = 1 Then "Oui"
      ELSE NULL END AS "Est décédé?",
@@ -35,7 +35,8 @@ DATE_FORMAT(DATE(personForDetails.death_date),'%d-%m-%Y') AS "Date de décés",
 cvForGettingDeathReason.concept_full_name AS "Raison de décés"
 
 from
-(select
+(
+select
 person_id,
 max(obsForAppointmentDate.obs_datetime) AS "obsDate",
 max(obsForAppointmentDate.value_datetime) as "valueDate",
@@ -54,15 +55,14 @@ where obsForAppointmentDate.concept_id in (
 AND obsForAppointmentDate.voided = 0
 AND visitForAppointmentDate.voided = 0
 group by obsForAppointmentDate.person_id
-)
-as A
+)as A
 INNER JOIN person personForDetails on A.person_id = personForDetails.person_id
 left JOIN concept_view cvForGettingDeathReason on personForDetails.cause_of_death = cvForGettingDeathReason.concept_id
 INNER JOIN patient_identifier identifier on A.person_id = identifier.patient_id AND identifier.voided = 0
 INNER JOIN person_name personalDetails ON A.person_id  = personalDetails.person_id
-left JOIN person_address personAddressDetails on personAddressDetails.person_id = A.person_id
+LEFT JOIN person_address personAddressDetails on personAddressDetails.person_id = A.person_id
 LEFT JOIN person_attribute personAttributeDetails on  A.person_id = personAttributeDetails.person_id AND personAttributeDetails.voided = 0
-LEFT JOIN person_attribute_type personAttributeTypeDetails ON  personAttributeDetails.person_attribute_type_id = personAttributeTypeDetails.person_attribute_type_id
+INNER JOIN person_attribute_type personAttributeTypeDetails ON  personAttributeDetails.person_attribute_type_id = personAttributeTypeDetails.person_attribute_type_id
 LEFT JOIN concept_view cvForAttribute on personAttributeDetails.value = cvForAttribute.concept_id
 
 where
