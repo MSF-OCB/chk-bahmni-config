870f4aa7979a99a7fea255309cd28f5114dd2ba1
vishnu <vishnu@indvishnu.local>
2018-05-31 21:49:38 +0530

    Vishnu|Pushing feedback changes for CD4 and Patient reg info report

--- openmrs/apps/reports/sql/CD4_Biochem.sql
+++ openmrs/apps/reports/sql/CD4_Biochem.sql
@@ -1,109 +1,109 @@
 Select
-*
- from(
-      Select  B.care_center_requesting AS "Provenance",
-       B.Patient_Name AS "Nom du patient",
-       B.Patient_Identifier AS "ID Patient",
-       EXTRACT(YEAR FROM Now()) - EXTRACT(YEAR FROM B.dob) as "Age"  ,
-       to_char(B.dob,'DD/MM/YYYY') AS "Date naissance",
-       B.sexe AS "Sexe",
-       to_char(B.sample_date, 'DD/MM/YYYY') as "Date de prélèvement",
+    *
+from(
+        Select  B.care_center_requesting AS "Provenance",
+                B.Patient_Name AS "Nom du patient",
+                B.Patient_Identifier AS "ID Patient",
+                EXTRACT(YEAR FROM Now()) - EXTRACT(YEAR FROM B.dob) as "Age"  ,
+                to_char(B.dob,'DD/MM/YYYY') AS "Date naissance",
+                B.sexe AS "Sexe",
+                to_char(B.sample_date, 'DD/MM/YYYY') as "Date de prélèvement",
 
-       sum(cast( CASE when B.CD4 = '' then null  ELSE B.CD4 END  AS NUMERIC)) AS "RESULTS CD4 (cells/µl)",
-       sum(cast( CASE when B.CD4_percentage = '' then null ELSE B.CD4_percentage END  AS NUMERIC)) AS "CD4 %",
-       sum(cast( CASE when B.GPT = '' then null ELSE B.GPT END AS NUMERIC)) AS "SGPT (UI/L)",
-       sum(cast( CASE when B.Creatinine = '' then null ELSE B.Creatinine END  AS NUMERIC)) AS "CREAT (µmol/L)",
-       sum(cast( CASE when B.Glucose_LCR = '' then null ELSE B.Glucose_LCR END  AS NUMERIC)) AS "GLU LCR (mg/dl)",
-       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Hep_B = '' then null ELSE B.Hep_B END AS NUMERIC))) ) "Hep. B", /*getting coded value for tests*/
-       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Crag_serique = '' then null  ELSE B.Crag_serique END AS NUMERIC))) )AS "CRAG SERIQUE",/*getting coded value for tests*/
-       (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Crag_LCR = '' then null  ELSE B.Crag_LCR END AS NUMERIC)))) AS "CRAG LCR",/*getting coded value for tests*/
+                sum(cast( CASE when B.CD4 = '' then null  ELSE B.CD4 END  AS NUMERIC)) AS "RESULTS CD4 (cells/µl)",
+                sum(cast( CASE when B.CD4_percentage = '' then null ELSE B.CD4_percentage END  AS NUMERIC)) AS "CD4 %",
+                sum(cast( CASE when B.GPT = '' then null ELSE B.GPT END AS NUMERIC)) AS "SGPT (UI/L)",
+                sum(cast( CASE when B.Creatinine = '' then null ELSE B.Creatinine END  AS NUMERIC)) AS "CREAT (µmol/L)",
+                sum(cast( CASE when B.Glucose_LCR = '' then null ELSE B.Glucose_LCR END  AS NUMERIC)) AS "GLU LCR (mg/dl)",
+                (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Hep_B = '' then null ELSE B.Hep_B END AS NUMERIC))) ) "Hep. B", /*getting coded value for tests*/
+                (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Crag_serique = '' then null  ELSE B.Crag_serique END AS NUMERIC))) )AS "CRAG SERIQUE",/*getting coded value for tests*/
+                (Select dict_entry from DICTIONARY where id in (sum(cast(CASE when B.Crag_LCR = '' then null  ELSE B.Crag_LCR END AS NUMERIC)))) AS "CRAG LCR",/*getting coded value for tests*/
 
-case
-when visit_type='OPD' AND priority=1 then 'OPD HIGH'
-when visit_type='IPD' AND priority=1 then 'IPD HIGH'
-when visit_type='OPD' AND priority=0  then 'OPD'
-when visit_type='IPD' AND priority=0  then 'IPD'
-when visit_type ='IPD'and priority = NULL then 'IPD'
-when visit_type='OPD' and priority=NULL then 'OPD'
+                case
+                when visit_type='OPD' AND priority=1 then 'OPD HIGH'
+                when visit_type='IPD' AND priority=1 then 'IPD HIGH'
+                when visit_type='OPD' AND priority=0  then 'OPD'
+                when visit_type='IPD' AND priority=0  then 'IPD'
+                when visit_type ='IPD'and priority = NULL then 'IPD'
+                when visit_type='OPD' and priority=NULL then 'OPD'
 
-end AS "Priority",
-comment
-
-
-FROM
-  (/*Pivoting the table row to column*/
-      SELECT
-       accession_number,
-       date_of_results,
-       Patient_Name,
-       care_center_requesting,
-       Patient_Identifier,
-       dob,
-       sexe,
-       sample_date,
-       comment,
+                end AS "Notes",
+            comment
 
 
+        FROM
+            (/*Pivoting the table row to column*/
+                SELECT
+                    accession_number,
+                    date_of_results,
+                    Patient_Name,
+                    care_center_requesting,
+                    Patient_Identifier,
+                    dob,
+                    sexe,
+                    sample_date,
+                    comment,
 
-       CASE
-           WHEN tname ='CD4' THEN tvalue
-       END AS CD4 ,
-       CASE
-           WHEN tname ='CD4 % (enfants de - 5 ans)' THEN tvalue
-       END AS CD4_percentage,
-       CASE
-           WHEN tname ='GPT' THEN tvalue
-       END AS GPT,
-       CASE
-           WHEN tname ='Creatinine' THEN tvalue
-       END AS Creatinine,
-       CASE
-           WHEN tname ='Glucose (LCR)' THEN tvalue
-       END AS Glucose_LCR,
-       CASE
-           WHEN tname ='Hépatite B' THEN tvalue
-       END AS Hep_B,
-       CASE
-           WHEN tname ='Crag serique' THEN tvalue
-       END AS Crag_serique,
-        CASE
-           WHEN tname ='Crag' THEN tvalue
-       END AS Crag_LCR,
-       priority,
-       visit_type
 
-       FROM
-         (/*defining columns*/
-           SELECT
-                sample.lastupdated  AS date_of_results,
-                ss.name AS care_center_requesting,
-                trim( concat( COALESCE(NULLIF(person.first_name, ''), ''), ' ', COALESCE(NULLIF(person.last_name, ''), '') ) ) AS Patient_Name,
-                pi.identity_data AS Patient_Identifier,
-                patient.birth_date :: DATE AS dob,
-                case when patient.gender = 'M' then 'H'
-                     when patient.gender = 'F' then 'F'
-                     when patient.gender = 'O' then 'A'
-                else
-                    patient.gender END AS sexe,
-                sample.collection_date as "sample_date",
-                t.name AS tname,
-                r.value AS tvalue,
-                sample.accession_number,
-                sample.priority,
-                sample.visit_type,
-                a.comment
-          FROM
-          patient_identity pi
-          INNER JOIN patient ON patient.id = pi.patient_id
-          INNER JOIN person ON patient.person_id = person.id
-          INNER JOIN sample_human ON patient.id = sample_human.patient_id
-          INNER JOIN sample sample on sample_human.samp_id=sample.id
-          INNER JOIN sample_source ss ON sample.sample_source_id = ss.id
-          INNER JOIN sample_item item ON sample.id = item.samp_id
-          INNER JOIN analysis a ON item.id = a.sampitem_id
-          INNER JOIN RESULT r ON a.id = r.analysis_id
-          INNER JOIN test t ON a.test_id = t.id
-          WHERE t.name IN (
+
+                    CASE
+                    WHEN tname ='CD4' THEN tvalue
+                    END AS CD4 ,
+                    CASE
+                    WHEN tname ='CD4 % (enfants de - 5 ans)' THEN tvalue
+                    END AS CD4_percentage,
+                    CASE
+                    WHEN tname ='GPT' THEN tvalue
+                    END AS GPT,
+                    CASE
+                    WHEN tname ='Creatinine' THEN tvalue
+                    END AS Creatinine,
+                    CASE
+                    WHEN tname ='Glucose (LCR)' THEN tvalue
+                    END AS Glucose_LCR,
+                    CASE
+                    WHEN tname ='Hépatite B' THEN tvalue
+                    END AS Hep_B,
+                    CASE
+                    WHEN tname ='Crag serique' THEN tvalue
+                    END AS Crag_serique,
+                    CASE
+                    WHEN tname ='Crag' THEN tvalue
+                    END AS Crag_LCR,
+                    priority,
+                    visit_type
+
+                FROM
+                    (/*defining columns*/
+                        SELECT
+                            sample.lastupdated  AS date_of_results,
+                            ss.name AS care_center_requesting,
+                            trim( concat( COALESCE(NULLIF(person.first_name, ''), ''), ' ', COALESCE(NULLIF(person.last_name, ''), '') ) ) AS Patient_Name,
+                            pi.identity_data AS Patient_Identifier,
+                            patient.birth_date  AS dob,
+                            case when patient.gender = 'M' then 'H'
+                            when patient.gender = 'F' then 'F'
+                            when patient.gender = 'O' then 'A'
+                            else
+                                patient.gender END AS sexe,
+                            sample.collection_date as "sample_date",
+                            t.name AS tname,
+                            r.value AS tvalue,
+                            sample.accession_number,
+                            sample.priority,
+                            sample.visit_type,
+                            a.comment
+                        FROM
+                            patient_identity pi
+                            INNER JOIN patient ON patient.id = pi.patient_id
+                            INNER JOIN person ON patient.person_id = person.id
+                            INNER JOIN sample_human ON patient.id = sample_human.patient_id
+                            INNER JOIN sample sample on sample_human.samp_id=sample.id
+                            INNER JOIN sample_source ss ON sample.sample_source_id = ss.id
+                            INNER JOIN sample_item item ON sample.id = item.samp_id
+                            INNER JOIN analysis a ON item.id = a.sampitem_id
+                            INNER JOIN RESULT r ON a.id = r.analysis_id
+                            INNER JOIN test t ON a.test_id = t.id
+                        WHERE t.name IN (
                             'CD4',
                             'CD4 % (enfants de - 5 ans)',
                             'GPT',
@@ -112,37 +112,37 @@ FROM
                             'Hépatite B',
                             'Crag serique',
                             'Crag'
-                          )
+                        )
 
-                AND a.status_id=6 /*Filtering the result which are validated*/
-                AND sample.accession_number IS NOT NULL
-                AND pi.identity_type_id = 2
+                              AND a.status_id=6 /*Filtering the result which are validated*/
+                              AND sample.accession_number IS NOT NULL
+                              AND pi.identity_type_id = 2
 
-                           ) AS A
-    ) AS B
-WHERE date(B.date_of_results) BETWEEN '#startDate#' and '#endDate#'
+                    ) AS A
+            ) AS B
+         WHERE date(B.date_of_results) BETWEEN '#startDate#' and '#endDate#'
 
-GROUP BY B.Patient_Name,
-         B.care_center_requesting,
-         B.Patient_Identifier,
-         B.dob,
-         B.sexe,
-         B.sample_date,
-         B.date_of_results,
-         B.accession_number,
-         B.priority,
-         B.visit_type,
-         B.comment
-
-
-ORDER BY B.date_of_results,
-         B.care_center_requesting,
-         B.Patient_Name,
-         B.dob,
-         B.sexe
+        GROUP BY B.Patient_Name,
+            B.care_center_requesting,
+            B.Patient_Identifier,
+            B.dob,
+            B.sexe,
+            B.sample_date,
+            B.date_of_results,
+            B.accession_number,
+            B.priority,
+            B.visit_type,
+            B.comment
 
 
-         ) as A
-         Where
-coalesce ("RESULTS CD4 (cells/µl)", "CD4 %","SGPT (UI/L)","CREAT (µmol/L)","GLU LCR (mg/dl)") is not null
-OR coalesce ("CRAG LCR","Hep. B","CRAG SERIQUE") is not null;
+        ORDER BY B.date_of_results,
+            B.care_center_requesting,
+            B.Patient_Name,
+            B.dob,
+            B.sexe
+
+
+    ) as A
+Where
+    coalesce ("RESULTS CD4 (cells/µl)", "CD4 %","SGPT (UI/L)","CREAT (µmol/L)","GLU LCR (mg/dl)") is not null
+    OR coalesce ("CRAG LCR","Hep. B","CRAG SERIQUE") is not null;
--- openmrs/apps/reports/sql/patient_reg_info.sql
+++ openmrs/apps/reports/sql/patient_reg_info.sql
@@ -1,55 +1,57 @@
 select distinct pi.identifier  as "ID Patient",
-              group_concat( distinct (case when pat.name='Type de cohorte' then c.name else NULL end)) as "Type Cohorte",
-              concat(pn.family_name,' ',ifnull(pn.middle_name,''),' ', ifnull(pn.given_name,'')) as Nom,
-              concat(floor(datediff(now(), p.birthdate)/365), ' ans, ',  floor((datediff(now(), p.birthdate)%365)/30),' mois') as "Age",
-              case when p.gender='M' then 'Homme' when p.gender='F' then 'Femme' else null end as Sexe,
-              date_format(p.birthdate, '%d/%m/%Y') as "Date de naissance",
-              date_format(p.date_created,'%d/%m/%Y') as "Date enregistrement",
-              group_concat( distinct  (  case when pat.name='Date entrée cohorte' then  date(pa.value)  else  null end )) As "Date entrée cohorte",
-               group_concat( distinct ( case when pat.name='Status VIH' then c.name else NULL end )) as "Status VIH",
-               group_concat( distinct ( case when pat.name like '%Date%epist%' then date(pa.value)  else null end )) as "Date dépistage",
-              group_concat( distinct  (  case when pat.name='ARV Naif/ Non Naif' then c.name else NULL end )) as "ARV Naif/ Non Naif",
-              group_concat( distinct  (  case when pat.name='Converti CP' then case when pa.value ='true' then 'Oui' else 'Non'  end else null end )) As "Est converti CP",
-               group_concat( distinct ( case when pat.name='Date de conversion' then date_format(pa.value,'%d/%m/%Y') else null end)) as "Date conversion",
-               group_concat(distinct (case when pat.name like '%B%xpos%' then case when pa.value='true' then 'Oui' else 'Non' end else null end ))as "Est bébé exposé",
-               group_concat(distinct( case when pi.identifier  then (select identifier from patient_identifier where patient_id = r2.person_b) else null end)) AS "Nro Mére(Bébé éxposé)",
-               case when p.dead=1 then 'Oui' when p.dead=0 then 'Non' else null end as "Est décédé",
-               date_format(p.death_date, '%d/%m/%Y') as "Date de décés",
-               group_concat(distinct (case when pat.name like '%Lieu%de%stage%' then pa.value else null end)) as "Lieu de dépistage",
-               group_concat(distinct (case when pat.name ='Categorie Centre de provenance' then c.name else null end)) as "Categorie Centre de provenance",
-               group_concat(distinct (case when pat.name='Centre de provenance' then pa.value else null end)) as "Centre de provenance",
-               pad.COUNTY_DISTRICT as "Commune",
-               pad.address4 as "Quartier",
-               pad.address3 as "Rue",
-               pad.address2 as "No",
-               group_concat(distinct (case when pat.name='Tel 1' then pa.value else null end)) as "Tel 1",
-               group_concat(distinct (case when pat.name='Tel 2' then pa.value else null end)) as "Tel 2",
-               group_concat(distinct (case when pat.name ='Etat Civil' then c.name else null end)) as "Eta Civil",
-               group_concat(distinct (case when pat.name='Situation Parental(enfants)' then c.name else null end)) as"Situation Parental",
-               group_concat(distinct (case when pat.name="Niveau d'etudes" then c.name else null end ))as "Niveau étude",
-               group_concat(distinct (case when pat.name='Occupation' then pa.value else null end)) as "Occupation",
-               group_concat(distinct (case when pat.name='Religion' then c.name else null end)) as "Réligion",
-               group_concat(distinct (case when pat.name like "%Membre%seau%PVVIH%" then c.name else null end)) as "Membre réseau PVVIH",
-               group_concat(distinct (case when pat.name like "%Resident%Kinshasa" then c.name else null end)) as "Rés. à Kinshasa",
-               group_concat(distinct (case when pat.name="Pays d'origine(autre que RDC)" then c.name else null end)) as "Pays origine",
-               group_concat(distinct (case when pat.name="Province de provenance" then c.name else null end)) as "Prov. De provenance",
-               group_concat(distinct (case when pat.name="Frequente un tradipraticien" then c.name else null end)) as "Fréquente tradipraticien",
-               group_concat(distinct (case when pat.name="Nom du contact" then pa.value else null end)) as "Nom contact",
-               group_concat(distinct (case when pat.name="Tel 1 du Contact" then pa.value else null end)) as "Tel 1 Contact",
-               group_concat(distinct (case when pat.name="Tel 2 du Contact" then pa.value else null end)) as "Tel 2 Contact"
-               from  patient_identifier pi
-                join person p on p.person_id=pi.patient_id
-                join person_name pn on pn.person_id=p.person_id
-               left join person_attribute pa on p.person_id=pa.person_id and pa.voided=0
+                group_concat( distinct (case when pat.name='Type de cohorte' then c.name else NULL end)) as "Type Cohorte",
+                concat(pn.family_name,' ',ifnull(pn.middle_name,''),' ', ifnull(pn.given_name,'')) as Nom,
+                concat(floor(datediff(now(), p.birthdate)/365), ' ans, ',  floor((datediff(now(), p.birthdate)%365)/30),' mois') as "Age",
+                case when p.gender='M' then 'Homme' when p.gender='F' then 'Femme' else null end as Sexe,
+                date_format(p.birthdate, '%d/%m/%Y') as "Date de naissance",
+                date_format(p.date_created,'%d/%m/%Y') as "Date enregistrement",
+                group_concat( distinct  (  case when pat.name='Date entrée cohorte' then  date(pa.value)  else  null end )) As "Date entrée cohorte",
+                group_concat( distinct ( case when pat.name='Status VIH' then c.name else NULL end )) as "Status VIH",
+                group_concat( distinct ( case when pat.name like '%Date%epist%' then date(pa.value)  else null end )) as "Date dépistage",
+                group_concat( distinct  (  case when pat.name='ARV Naif/ Non Naif' then c.name else NULL end )) as "ARV Naif/ Non Naif",
+                group_concat( distinct  (  case when pat.name='Converti CP' then case when pa.value ='true' then 'Oui' else 'Non'  end else null end )) As "Est converti CP",
+                group_concat( distinct ( case when pat.name='Date de conversion' then date_format(pa.value,'%d/%m/%Y') else null end)) as "Date conversion",
+                group_concat(distinct (case when pat.name like '%B%xpos%' then case when pa.value='true' then 'Oui' else 'Non' end else null end ))as "Est bébé exposé",
+                group_concat(distinct( case when pi.identifier  then (select identifier from patient_identifier where patient_id = r2.person_b) else null end)) AS "Nro Mére(Bébé éxposé)",
+                case when p.dead=1 then 'Oui' when p.dead=0 then 'Non' else null end as "Est décédé",
+                date_format(p.death_date, '%d/%m/%Y') as "Date de décés",
+                cn_death.name as "Raison du décés",
+                group_concat(distinct (case when pat.name like '%Lieu%de%stage%' then pa.value else null end)) as "Lieu de dépistage",
+                group_concat(distinct (case when pat.name ='Categorie Centre de provenance' then c.name else null end)) as "Categorie Centre de provenance",
+                group_concat(distinct (case when pat.name='Centre de provenance' then pa.value else null end)) as "Centre de provenance",
+                pad.COUNTY_DISTRICT as "Commune",
+                pad.address4 as "Quartier",
+                pad.address3 as "Rue",
+                pad.address2 as "No",
+                group_concat(distinct (case when pat.name='Tel 1' then pa.value else null end)) as "Tel 1",
+                group_concat(distinct (case when pat.name='Tel 2' then pa.value else null end)) as "Tel 2",
+                group_concat(distinct (case when pat.name ='Etat Civil' then c.name else null end)) as "Eta Civil",
+                group_concat(distinct (case when pat.name='Situation Parental(enfants)' then c.name else null end)) as"Situation Parental",
+                group_concat(distinct (case when pat.name="Niveau d'etudes" then c.name else null end ))as "Niveau étude",
+                group_concat(distinct (case when pat.name='Occupation' then pa.value else null end)) as "Occupation",
+                group_concat(distinct (case when pat.name='Religion' then c.name else null end)) as "Réligion",
+                group_concat(distinct (case when pat.name like "%Membre%seau%PVVIH%" then c.name else null end)) as "Membre réseau PVVIH",
+                group_concat(distinct (case when pat.name like "%Resident%Kinshasa" then c.name else null end)) as "Rés. à Kinshasa",
+                group_concat(distinct (case when pat.name="Pays d'origine(autre que RDC)" then c.name else null end)) as "Pays origine",
+                group_concat(distinct (case when pat.name="Province de provenance" then c.name else null end)) as "Prov. De provenance",
+                group_concat(distinct (case when pat.name="Frequente un tradipraticien" then c.name else null end)) as "Fréquente tradipraticien",
+                group_concat(distinct (case when pat.name="Nom du contact" then pa.value else null end)) as "Nom contact",
+                group_concat(distinct (case when pat.name="Tel 1 du Contact" then pa.value else null end)) as "Tel 1 Contact",
+                group_concat(distinct (case when pat.name="Tel 2 du Contact" then pa.value else null end)) as "Tel 2 Contact"
+from  patient_identifier pi
+    join person p on p.person_id=pi.patient_id
+    join person_name pn on pn.person_id=p.person_id
+    left join person_attribute pa on p.person_id=pa.person_id and pa.voided=0
 
-               left join person_attribute_type pat on  pa.person_attribute_type_id=pat.person_attribute_type_id
+    left join person_attribute_type pat on  pa.person_attribute_type_id=pat.person_attribute_type_id
 
-               left join person_address pad on pad.person_id=p.person_id
-                left  join relationship r on r.person_b=pi.patient_id
-                left  join relationship r2 on r2.person_a=pi.patient_id
-               left join concept_name c on c.concept_id=pa.value and c.voided = 0 and c.locale_preferred=1
-               where date(case when pat.name='Date entrée cohorte' then  date(pa.value)  else  null end )
+    left join person_address pad on pad.person_id=p.person_id
+    left  join relationship r on r.person_b=pi.patient_id
+    left  join relationship r2 on r2.person_a=pi.patient_id
+    left join concept_name c on c.concept_id=pa.value and c.voided = 0 and c.locale_preferred=1
+    left outer join concept_name cn_death on cn_death.concept_id = p.cause_of_death AND cn_death.locale_preferred = 1 AND cn_death.voided IS FALSE AND p.voided IS FALSE
+where date(case when pat.name='Date entrée cohorte' then  date(pa.value)  else  null end )
 
+BETWEEN date('#startDate#') and Date('#endDate#')
 
- BETWEEN date('#startDate#') and Date('#endDate#')
-              group by pi.identifier,r.person_b;
+group by pi.identifier,r.person_b;
