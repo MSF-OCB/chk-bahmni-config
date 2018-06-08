e6a2cff38585943bd2544fab40231269d8d67a8c
vishnu <vishnu@indvishnu.local>
2018-06-08 12:57:29 +0530

    Vishnu|correcting Syphilis labtest name in report

--- openmrs/apps/reports/sql/many_test_report.sql
+++ openmrs/apps/reports/sql/many_test_report.sql
@@ -180,7 +180,7 @@ FROM
                                                    WHEN tname ='Hemoglobine' then tvalue
                                                END AS HEMO,
                                                CASE
-                                                   WHEN tname ='Syphills' then tvalue
+                                                   WHEN tname ='Syphilis' then tvalue
                                                END AS SYPHILLS,
                                                CASE
                                                    WHEN tname ='Glycémie' then tvalue
@@ -242,7 +242,7 @@ FROM
             'Microscopie TB - Recherche de BAAR (Pleural)','Microscopie TB - Recherche de BAAR (Crachat)','Microscopie TB - Recherche de BAAR (LCR-TB)',
             'Microscopie TB - Recherche de BAAR (LCR)','Microscopie TB - Recherche de BAAR (Urine)','Proteinorachie (test de Pandy)',
             'FL - NEUT%','FL - MXD%','FL - LYM%','Rivalta (Ganglionnaire)','Hemoglobine* (Hemocue)','Hemoglobine',
-            'Lactate','Globules Blancs','Syphills','Malaria','Glycémie')
+            'Lactate','Globules Blancs','Syphilis','Malaria','Glycémie')
 
        WHERE   a.status_id=6 /*Filtering the result which are validated*/
                        AND sample.accession_number IS NOT NULL
