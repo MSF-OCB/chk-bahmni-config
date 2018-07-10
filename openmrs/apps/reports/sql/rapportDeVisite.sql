58688beff313a0751f8de18711fa8ec7aa2be924
matirp235 <dpritam@thoughtworks.com>
2018-07-10 18:29:19 +0530

    Pritam | Feedback on visit report and dashboard Antécédents cliniques translation

--- openmrs/apps/reports/sql/rapportDeVisite.sql
+++ openmrs/apps/reports/sql/rapportDeVisite.sql
@@ -27,77 +27,76 @@ FROM visit v
   INNER JOIN person p ON p.person_id = pi.patient_id AND p.voided IS FALSE
   INNER JOIN person_name pn ON pn.person_id = pi.patient_id AND pn.voided IS FALSE
   LEFT JOIN (
-              SELECT
-                person_id,
-                cv.concept_full_name
-              FROM person_attribute pa
-                INNER JOIN person_attribute_type pat
-                  ON pat.person_attribute_type_id = pa.person_attribute_type_id AND pa.voided IS FALSE AND
-                     pat.retired IS FALSE
-                INNER JOIN concept_view cv ON cv.concept_id = pa.value AND cv.retired IS FALSE
-              WHERE pat.name = 'Type de cohorte'
+                  SELECT
+                    person_id,
+                    cv.concept_full_name
+                  FROM person_attribute pa
+                    INNER JOIN person_attribute_type pat
+                      ON pat.person_attribute_type_id = pa.person_attribute_type_id AND pa.voided IS FALSE AND
+                         pat.retired IS FALSE
+                    INNER JOIN concept_view cv ON cv.concept_id = pa.value AND cv.retired IS FALSE
+                  WHERE pat.name = 'Type de cohorte'
             ) typeDCohorte ON typeDCohorte.person_id = pi.patient_id
   LEFT JOIN (
-              SELECT
-                person_id,
-                pa.value
-              FROM person_attribute pa
-                INNER JOIN person_attribute_type pat
-                  ON pat.person_attribute_type_id = pa.person_attribute_type_id AND pa.voided IS FALSE AND
-                     pat.retired IS FALSE
-              WHERE pat.name = 'Date entrée cohorte'
+                  SELECT
+                    person_id,
+                    pa.value
+                  FROM person_attribute pa
+                    INNER JOIN person_attribute_type pat
+                      ON pat.person_attribute_type_id = pa.person_attribute_type_id AND pa.voided IS FALSE AND
+                         pat.retired IS FALSE
+                  WHERE pat.name = 'Date entrée cohorte'
             ) dateEntreeCohore ON dateEntreeCohore.person_id = pi.patient_id
-  LEFT JOIN (
-              SELECT
-               v.patient_id,
-               v.visit_id,
+  LEFT JOIN (/*getting consulation name*/
+                  SELECT
+                   v.patient_id,
+                   v.visit_id,
 
-                GROUP_CONCAT(DISTINCT (concat_ws(' ', pn.given_name, pn.family_name))) AS 'names'
+                    GROUP_CONCAT(DISTINCT (concat_ws(' ', pn.given_name, pn.family_name))) AS 'names'
 
 
-             FROM visit v INNER JOIN encounter e ON e.visit_id = v.visit_id
-             inner join obs o on o.encounter_id=e.encounter_id and o.person_id =v.patient_id
-             inner join encounter_provider ep on ep.encounter_id=e.encounter_id
-             inner join provider pr on pr.provider_id=ep.provider_id
-             inner join person_name pn on pn.person_id=pr.person_id
-
-             group by patient_id,
-              v.visit_id) consultant_name ON consultant_name.visit_id = v.visit_id
-  LEFT JOIN (
-              SELECT
-                obs.person_id,
-                obs.value_datetime,
-                latest_obs.visit_id
-              FROM obs obs
-                INNER JOIN encounter en
-                  ON en.encounter_id = obs.encounter_id AND obs.voided IS FALSE AND en.voided IS FALSE
-                INNER JOIN concept_view cv
-                  ON cv.concept_id = obs.concept_id AND
-                     cv.concept_full_name IN ('Date de prochain RDV', 'Date du prochain RDV') AND
-                     cv.retired IS FALSE
-                INNER JOIN (SELECT
-                              pv.visit_id,
-                              pv.prev_visit_id,
-                              MAX(o.obs_datetime) AS obsDateTime
-                            FROM obs o
-                              INNER JOIN encounter e
-                                ON e.encounter_id = o.encounter_id AND o.voided IS FALSE AND e.voided IS FALSE
-                              INNER JOIN concept_view cv
-                                ON cv.concept_id = o.concept_id AND
-                                   cv.concept_full_name IN ('Date de prochain RDV', 'Date du prochain RDV') AND
-                                   cv.retired IS FALSE
-                              INNER JOIN (SELECT
-                                            v.patient_id,
-                                            v.visit_id,
-                                            max(prev_visit.visit_id) AS prev_visit_id
-                                          FROM
-                                            visit v
-                                            LEFT JOIN
-                                            visit prev_visit ON v.visit_id > prev_visit.visit_id AND
-                                                                v.patient_id = prev_visit.patient_id
-                                          GROUP BY v.visit_id) pv ON pv.prev_visit_id = e.visit_id
-                            GROUP BY pv.visit_id) latest_obs
-                  ON latest_obs.prev_visit_id = en.visit_id AND latest_obs.obsDateTime = obs.obs_datetime
-            ) prev_appt_date
-    ON prev_appt_date.visit_id = v.visit_id
+                 FROM visit v INNER JOIN encounter e ON e.visit_id = v.visit_id
+                 inner join obs o on o.encounter_id=e.encounter_id and o.person_id =v.patient_id
+                 inner join encounter_provider ep on ep.encounter_id=e.encounter_id
+                 inner join provider pr on pr.provider_id=ep.provider_id
+                 inner join person_name pn on pn.person_id=pr.person_id
+                 group by patient_id,v.visit_id
+              ) consultant_name ON consultant_name.visit_id = v.visit_id
+             LEFT JOIN (/*getting appointment date of the previous visit*/
+                        SELECT
+                        obs.person_id,
+                        obs.value_datetime,
+                        latest_obs.visit_id
+                        FROM obs obs
+                        INNER JOIN encounter en ON en.encounter_id = obs.encounter_id AND obs.voided IS FALSE AND en.voided IS FALSE
+                        INNER JOIN concept_view cv ON cv.concept_id = obs.concept_id
+                        AND cv.concept_full_name IN ('Date de prochain RDV', 'Date du prochain RDV') AND cv.retired IS FALSE
+                        INNER JOIN (
+                                    SELECT
+                                    pv.visit_id,
+                                    pv.prev_visit_id,
+                                    MAX(o.obs_datetime) AS obsDateTime
+                                    FROM obs o
+                                    INNER JOIN encounter e
+                                    ON e.encounter_id = o.encounter_id AND o.voided IS FALSE AND e.voided IS FALSE
+                                    INNER JOIN concept_view cv
+                                    ON cv.concept_id = o.concept_id AND
+                                    cv.concept_full_name IN ('Date de prochain RDV', 'Date du prochain RDV') AND
+                                    cv.retired IS FALSE
+                                    INNER JOIN (/*Getting previous visit*/
+                                                SELECT
+                                                v.patient_id,
+                                                v.visit_id,
+                                                max(prev_visit.visit_id) AS prev_visit_id
+                                                FROM
+                                                visit v
+                                                LEFT JOIN
+                                                visit prev_visit ON DATE(v.date_started) > DATE(prev_visit.date_started) /*comparing current visit date with previous visit date*/
+                                                                  AND v.patient_id = prev_visit.patient_id
+                                                GROUP BY v.visit_id
+                                                ) pv ON pv.prev_visit_id = e.visit_id
+                                    GROUP BY pv.visit_id
+                                    ) latest_obs ON latest_obs.prev_visit_id = en.visit_id AND latest_obs.obsDateTime = obs.obs_datetime
+                        ) prev_appt_date
+    ON prev_appt_date.visit_id = v.visit_id 
 WHERE DATE(v.date_started) BETWEEN DATE('#startDate#') AND DATE('#endDate#');
